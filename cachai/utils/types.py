6911ee26533a9b089015ad3749237d4ab628937b
Farhad Ghayour <me@farhadg.com>
2024-10-24 16:37:04 +0800

    create types file for models

--- cachai/utils/types.py
+++ cachai/utils/types.py
@@ -1,7 +1,13 @@
+from pydantic import BaseModel
+from typing import Literal, Type
 from dataclasses import dataclass
 from enum import Enum
 
 import cachai.utils.constants as C
+
+"""
+general types
+"""
 
 
 class ObservationType(Enum):
@@ -9,3 +15,28 @@ class ObservationType(Enum):
     MISS = C.MISS
     STALE = C.STALE
     VALID_TTL = C.VALID_TTL
+
+
+"""
+experiment config definitions
+"""
+
+
+class ExperimentConfig(BaseModel):
+    iterations: int
+    debug: bool = False
+
+
+class SimulatorConfig(BaseModel):
+    type: Literal['TTLSimulator']
+
+
+class StrategyConfig(BaseModel):
+    type: Literal['DebuggerStrategy']
+
+
+class Experiment(BaseModel):
+    experiment_name: str
+    experiment_config: ExperimentConfig
+    simulator_config: SimulatorConfig
+    strategy_config: StrategyConfig
